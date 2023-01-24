48ff7414a508d8a08d3897ae8c93df305fba8f61
Jimena Palmero <jimena.palmero@macena.com.ar>
2023-01-23 22:46:49 -0300

    Se sube el proyecto

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script >
+import MapaGlobe from "./components/MapaGlobe.vue"
+
+export default {
+  components:{
+    MapaGlobe
+  }
+}
+</script>
+
+<template>
+<MapaGlobe/>
+</template>
+
+<style scoped>
+html, body {
+  padding: 0;
+  margin: 0;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/MapaGlobe.vue
@@ -0,0 +1,32 @@
+<template>
+<h1>Bienvenido al mapa global</h1>
+<div ref="mapContainer" class="map-container"></div>
+   <div id='map' style='width: 600x; height: 400px;'></div>
+</template>
+<script setup>
+import 'mapbox-gl/dist/mapbox-gl.css';
+import {onMounted, ref} from 'vue';
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamltZW5hbWFwOTkiLCJhIjoiY2xkOWg1MHF1MDcwcjNvczl3MWlmYWo0YSJ9.4JljQunKYrxQkJZF8XjAHA';
+
+const mapContainer = ref<HTMLElement>(null)
+onMounted(()=>{
+
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 1, // starting zoom
+    });
+    
+})
+
+
+</script>
+<style scoped>
+.map-container{
+    width: 100vw;
+}
+body { margin: 0; padding: 0; }
+</style>
